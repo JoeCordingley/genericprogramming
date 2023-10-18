37d578f7d0f726dea2915cde680f568966a3bae5
Joe Cordingley <joewcordingley@gmail.com>
2023-10-18 18:33:42 +0100

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import scala.deriving.Mirror
 import cats.syntax.all.*
+import cats.*
 
 enum Json:
   case JsonString(s: String)
@@ -72,6 +73,9 @@ trait JsonMemberEncoder[A]:
 trait JsonMemberDecoder[A]:
   def decode(pairs: Map[String, Json]): Option[A] 
 
+trait JsonMemberDecoder2[A, B]:
+  def decode(pairs: Map[String, Json]): Either[A, B] 
+
 given JsonMemberEncoder[EmptyTuple] = _ => List.empty
 given JsonMemberDecoder[EmptyTuple] = _ => Some(EmptyTuple)
 
@@ -92,16 +96,55 @@ given [K: JsonFieldEncoder, V: Decoder]: JsonMemberDecoder[JsonOptionalMember[K,
 
 given [L: Decoder, R: Decoder]: Decoder[Either[L, R]] = j => summon[Decoder[L]].decode(j).map(Left(_)).orElse(summon[Decoder[R]].decode(j).map(Right(_)))
 
-given [A: JsonMemberDecoder, T <: Tuple: JsonMemberDecoder]: JsonMemberDecoder[A *: T] = map => for {
-  v <- summon[JsonMemberDecoder[A]].decode(map)
-  t <- summon[JsonMemberDecoder[T]].decode(map)
-} yield v *: t
+given [A: JsonMemberDecoder, T <: Tuple: JsonMemberDecoder]: JsonMemberDecoder[A *: T] = map => 
+  (summon[JsonMemberDecoder[A]].decode(map), summon[JsonMemberDecoder[T]].decode(map)).mapN(_ *: _)
 
-given [L: Encoder, R: Encoder]: Encoder[Either[L, R]] = {
+given [A, B, T <: Tuple, U <: Tuple](using j: JsonMemberDecoder2[A, B], k: JsonMemberDecoder2[T, (T, U)]): JsonMemberDecoder2[Either[A, B] *: T, (Either[A, B] *: T, B *: U)] = map => joinEither(summon[JsonMemberDecoder2[A, B]].decode(map), summon[JsonMemberDecoder2[T, (T, U)]].decode(map))
+
+type JsonError[A]
+
+def joinEither[A, B, T <: Tuple, U <: Tuple](l: Either[A, B], r: Either[T, (T, U)]): Either[Either[A, B] *: T, (Either[A, B] *: T, B *: U)] = (l, r) match {
+  case (Left(a), Left(t)) => Left(Left(a) *: t)
+  case (Left(a), Right((t, _))) => Left(Left(a) *: t)
+  case (Right(b), Left(t)) => Left(Right(b) *: t)
+  case (Right(b), Right((t, u))) => Right(Right(b) *: t, b *: u)
+}
+
+//given [A: JsonMemberDecoder, T <: Tuple: JsonMemberDecoder]: JsonMemberDecoder[A *: T] = map => 
+//  Applicative[Option].map2(summon[JsonMemberDecoder[A]].decode(map), summon[JsonMemberDecoder[T]].decode(map))(_ *: _)
+
+//given [A: JsonMemberDecoder, T <: Tuple: JsonMemberDecoder]: JsonMemberDecoder[A *: T] = map => 
+//  Applicative[Option].ap[T, A *: T](summon[JsonMemberDecoder[A]].decode(map).map(a => b => a *: b))(summon[JsonMemberDecoder[T]].decode(map))
+
+given [L: Encoder, R: Encoder]: Encoder[L /: R] = {
   case Left(l) => summon[Encoder[L]].encode(l)
   case Right(r) => summon[Encoder[R]].encode(r)
 }
 
+type /:[L, R] = Either[L, R]
+
+def map2[F[_]: Apply, A, B, C](fa: F[A], fb: F[B], f: (A, B) => C): F[C] = Apply[F].ap(Apply[F].map(fa)(f.curried))(fb)
+
+def ap[F[_]: Apply, A, B](ff: F[A => B], fa: F[A]): F[B] = Apply[F].map2(ff, fa)(_(_))
+
+//trait ApplicativeF[F[_[_], _]]:
+//  def map2[G[_], A, B, C](fa: F[G, A], fb: F[G, B])(f: (G[A], G[B]) => G[C]): F[G, C]
+
+type EitherF[F[_], A] = Either[F[A], A]
+
+//given ApplicativeF[EitherF] with
+//  def map2[G[_], A, B, C](fa: EitherF[G, A], fb: EitherF[G, B])(f: (G[A], G[B]) => G[C]): EitherF[G, C] = (fa, fb) match {
+//    case (Left(ga), Left(gb)) => Left(f(ga, gb))
+//  }
+
+
+
+//def joinEither[](fa: Either[L, R], ft: Either[, T]): Either = (fa, ft) match {
+//  case (Left(a), Left(b)) => Left(Left(a) *: b)
+//  case (Left(a), Right((b, _))) => Left(Left(a) *: b)
+//  case (Right(a), Left(b)) => Left(Right(a) *: b)
+//  case (Right(a), Right((b, c)) => Right(Right(a) *: b, (a *: c))
+//}
 
 //
 //  type ProductOfEs[T, E] = Mirror.Product { type MirroredType = T; type MirroredMonoType = T ; type MirroredElemTypes = E}
